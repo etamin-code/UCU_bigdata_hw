faefe062d3dba4a01b94d4bfb0f2412aaa0cd372
etamin-code <70692373+etamin-code@users.noreply.github.com>
2022-06-24 19:24:07 +0300

    Update run_producer.sh

--- lab_7/run_producer.sh
+++ lab_7/run_producer.sh
@@ -3,4 +3,4 @@
 declare -r NETWORK="kafka-network"
 
 docker build -f dockerfiles/producer.Dockerfile --tag kafka_producer .
-docker run  --rm --name producer --network ${NETWORK} kafka_producer
+docker run  -d --rm --name producer --network ${NETWORK} kafka_producer
